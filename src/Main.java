0a414f310c86e953489a7e5abf160ef4c23941ca
AlanChow <@alanc118@nycstudents.net>
2025-11-17 14:14:08 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 52: ");
+        int start = scan.nextInt();
+        int cards = Cards.cards(start);
+        System.out.println("Enter your guess from 1 to 52");
+        Cards guess = new Cards();
+        int guessOne = scan.nextInt();
+        if (guess.guesses() == guessOne)
+        {
+            System.out.println("Congratulations you guessed the " + " correctly!");
+        }
+
+    }
+}
